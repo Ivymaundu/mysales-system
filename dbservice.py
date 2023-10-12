48490068fa64a1fc37db66bcb3e35922bb5ccf13
Ivymaundu <ivymaundu03@gmail.com>
2023-10-12 22:45:13 +0300

    base

---
+++ dbservice.py
@@ -0,0 +1,59 @@
+import psycopg2
+from flask import request 
+try:
+    conn = psycopg2.connect(
+        database="myduka_class", user='postgres', password='12345', host='127.0.0.1', port= '5432')
+except:
+    print('unable to connect to the database')
+
+def get_data(my_data):
+    cur = conn.cursor()
+    t = "select * from "+my_data
+    cur.execute(t)
+    data = cur.fetchall()
+    return data
+prods=get_data('products')
+sales=get_data('sales')
+
+def add_product(values):
+    cur = conn.cursor()
+    insert_product="INSERT INTO products(name,buying_price,selling_price,stock_quantity) values(%s,%s,%s,%s)"
+    cur.execute(insert_product,values)
+    conn.commit()
+
+def add_sale(values):
+    cur=conn.cursor()
+    insert_sale="INSERT INTO sales(pid,quantity,created_at) values(%s,%s,now())"
+    cur.execute(insert_sale,values)
+    conn.commit()
+
+def create_user(values):
+    cursor = conn.cursor()
+    insert_query = "INSERT INTO users (full_name, email, password) values (%s, %s, %s)"
+    cursor.execute(insert_query,values)
+    conn.commit()
+
+def check_email_exists(email):
+    cursor = conn.cursor()
+    query = "SELECT EXISTS(SELECT 1 FROM users WHERE email = %s)"
+    cursor.execute(query, (email,))
+    exists = cursor.fetchone()[0]
+    return exists
+
+def check_email_password_match(email, password):
+    cursor = conn.cursor()
+    query = "SELECT id FROM users WHERE email = %s AND password = %s"
+    cursor.execute(query, (email, password))
+    result = cursor.fetchone()
+    if result is not None:
+        id = result[0]
+        return id
+    else:
+        return None
+
+def calc_profit():
+    cur=conn.cursor()
+    find_profit="SELECT date(created_at) as days,sum((products.selling_price-products.buying_price)*sales.quantity) as profit from sales join products on products.id=sales.pid group by days order by days;"
+    cur.execute(find_profit)
+    profit_per_day=cur.fetchall()
+    return profit_per_day
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import psycopg2
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,session
+from dbservice import get_data,add_product,create_user,check_email_password_match,add_sale,calc_profit
 try:
     conn = psycopg2.connect(
         database="myduka_class", user='postgres', password='12345', host='127.0.0.1', port= '5432')
@@ -10,8 +11,77 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("profit.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/products")
+def products():
+    myprods = get_data("products")   
+    return render_template("products.html", myprods = myprods)
+
+@app.route("/add-products", methods=["Post"])
+def addproduct():
+        name           =  request.form["name"]
+        buying_price   =  request.form["buying_price"]
+        selling_price  =  request.form["selling_price"]
+        stock_quantity =  request.form["stock_quantity"]
+        values = (name,buying_price,selling_price,stock_quantity)
+
+        add_product(values)
+
+        return redirect("/products")
+
+@app.route("/sales")
+def sales():
+    products= get_data("products")  
+    sales= get_data("sales") 
+
+    return render_template("sales.html", myprods = products, mysales=sales)
+
+@app.route("/add-sales", methods=["Post"])
+def addsale():
+        pid = request.form["pid"]
+        quantity= request.form["quantity"]
+        values = (pid,quantity)
+
+        add_sale(values)
+
+        return redirect("/sales")
+
+@app.route("/register", methods= ["POST"])
+def create_account():
+    if request.method=="POST":
+        name=request.form["full_name"]
+        email=request.form["email"]
+        password=request.form["password"]
+        values=(name,email,password)
+        create_user(values)
+        return redirect("/login")
+    return render_template("register.html")
+
+@app.route("/login", methods=["POST","GET"])
+def access_account():
+        email = request.form["email"]
+        password = request.form["password"]
+       
+        check_email_password_match(email,password)
+        return  render_template("/dashboard.html")
+
+@app.route("/profit")
+def profit():
+    dates=[]
+    profits=[]
+    for i in calc_profit():
+        dates.append(str(i[0]))
+        profits.append(float(i[1]))
+    return render_template("profit.html",dates=dates,profits=profits)
 
 app.run(debug=True)
 
